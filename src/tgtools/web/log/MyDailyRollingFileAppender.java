4e043ac774f2847ec0de0945abc556f21de323bd
田径 <tian___jing@hotmail.com>
2017-05-27 13:05:51 +0800

    增加 日志文件自动补充地址

---
+++ src/tgtools/web/log/MyDailyRollingFileAppender.java
@@ -0,0 +1,52 @@
+package tgtools.web.log;
+
+import org.apache.log4j.DailyRollingFileAppender;
+import org.apache.log4j.helpers.LogLog;
+import org.apache.log4j.spi.LoggingEvent;
+import tgtools.util.LogHelper;
+import tgtools.util.StringUtil;
+import tgtools.web.platform.Platform;
+
+import java.io.IOException;
+
+/**
+ * 名  称：
+ * 编写者：田径
+ * 功  能：
+ * 时  间：12:03
+ */
+public class MyDailyRollingFileAppender extends DailyRollingFileAppender {
+    public MyDailyRollingFileAppender()
+    {
+    }
+    @Override
+    public void setFile(String file)
+    {
+        LogHelper.info("","设置日志地址："+file,"MyDailyRollingFileAppender");
+        super.setFile(file);
+        initFile();
+    }
+    private Boolean m_InitFile=false;
+    public void initFile()
+    {
+        if(m_InitFile)
+        {
+            return ;
+        }
+        LogHelper.info("","日志地址开始："+super.fileName,"MyDailyRollingFileAppender");
+        String path=Platform.getServerPath();
+        if(StringUtil.isNullOrEmpty(path))
+        {
+            return ;
+
+        }
+        if(super.fileName.indexOf(path)>=0)
+        {
+            return ;
+        }
+        super.fileName=path+super.fileName;
+        LogHelper.info("","日志地址结束："+super.fileName,"MyDailyRollingFileAppender");
+        m_InitFile=true;
+    }
+
+}
