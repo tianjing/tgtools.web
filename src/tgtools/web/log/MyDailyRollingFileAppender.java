bdfa3e8e56ce41a046a11004350855815623b523
田径 <tian___jing@hotmail.com>
2017-12-10 13:23:24 +0800

    修复 log 路径问题

--- src/tgtools/web/log/MyDailyRollingFileAppender.java
+++ src/tgtools/web/log/MyDailyRollingFileAppender.java
@@ -15,11 +15,14 @@ public class MyDailyRollingFileAppender extends DailyRollingFileAppender {
     public MyDailyRollingFileAppender()
     {
     }
+    private String mFile="";
     @Override
     public void setFile(String file)
     {
+        if(StringUtil.isNullOrEmpty(mFile)) {
+            mFile = file;
+        }
         LogHelper.info("","设置日志地址："+file,"MyDailyRollingFileAppender");
-        super.setFile(file);
         initFile();
     }
     private Boolean m_InitFile=false;
@@ -36,11 +39,20 @@ public class MyDailyRollingFileAppender extends DailyRollingFileAppender {
             return ;
 
         }
-        if(super.fileName.indexOf(path)>=0)
+        if(StringUtil.isNullOrEmpty(mFile)||mFile.indexOf(path)>=0)
         {
             return ;
         }
-        super.fileName=path+super.fileName;
+
+        if(mFile.indexOf("/")==0||mFile.indexOf("\\")==0)
+        {
+            mFile=mFile.substring(1);
+        }
+        path =path+mFile;
+        path=StringUtil.replace(path,"\\","/");
+        path=StringUtil.replace(path,"//","/");
+        super.fileName=path;
+        super.setFile(super.fileName);
         LogHelper.info("","日志地址结束："+super.fileName,"MyDailyRollingFileAppender");
         m_InitFile=true;
     }
